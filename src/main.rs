111a0f58ef9ce0e8b0f2f9a85fa0214231e5be45
richardanyalai <gtaricsi@gmail.com>
2025-11-06 09:59:38 +0100

    feat: step_2a

--- src/main.rs
+++ src/main.rs
@@ -40,6 +40,9 @@ async fn main() -> Result {
     let out_dir = PathBuf::from("./out");
     let keys_dir = out_dir.join("keys");
     let ids_dir = out_dir.join("ids");
+    let step_2_dir = out_dir.join("step_2");
+    let step_2a_dir = out_dir.join("step_2a");
+    let step_2a_signed_dir = step_2a_dir.join("signed-proposals");
 
     // Create directories if they don't exist
     if !out_dir.exists() {
@@ -54,20 +57,12 @@ async fn main() -> Result {
 
     // Execute the requested command
     match args.command {
-        Commands::All => {
-            steps::run_all_steps(&config, &dars_dir, &keys_dir, &ids_dir, &out_dir).await?
-        }
+        Commands::All => steps::run_all_steps(&config, &dars_dir, &keys_dir, &ids_dir, &out_dir).await?,
         Commands::UploadDars => steps::upload_dars(&config, &dars_dir).await?,
         Commands::GenerateKeys => steps::generate_keys(&config, &keys_dir, &ids_dir).await?,
-        Commands::CreateProposals => {
-            steps::create_proposals(&config, &keys_dir, &ids_dir, &out_dir).await?
-        }
-        Commands::SignDnsProposals => {
-            let step_2_dir = out_dir.join("step_2");
-            let step_2a_signed_dir = out_dir.join("step_2a").join("signed-proposals");
-            steps::sign_dns_proposals(&config, &step_2_dir, &step_2a_signed_dir, &ids_dir).await?
-        }
-        Commands::SubmitDnsProposals => steps::submit_dns_proposals().await?,
+        Commands::CreateProposals => steps::create_proposals(&config, &keys_dir, &ids_dir, &out_dir).await?,
+        Commands::SignDnsProposals => steps::sign_dns_proposals(&config, &step_2_dir, &step_2a_signed_dir, &ids_dir).await?,
+        Commands::SubmitDnsProposals => steps::submit_dns_proposals(&config, &step_2_dir, &step_2a_dir).await?,
         Commands::SignP2pPtkProposals => steps::sign_p2p_ptk_proposals().await?,
         Commands::SubmitFinalProposals => steps::submit_final_proposals().await?,
         Commands::PrepareSubmissions => steps::prepare_submissions().await?,
